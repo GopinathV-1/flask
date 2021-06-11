e32ddc9de07e6f6f950d053bb1681e7a24cf7a60
Gopinath V <57871800+GopinathV-1@users.noreply.github.com>
2021-06-11 20:40:37 +0530

    Update __init__.py

--- studentproj/__init__.py
+++ studentproj/__init__.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
@@ -26,8 +26,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('USER_MAIL')
-app.config['MAIL_PASSWORD'] = os.getenv('USER_PASSWORD')
+app.config['MAIL_USERNAME'] = 'gopinathvcarrier@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Vgopinath190@'
 mail = Mail(app)
 
 
