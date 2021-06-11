cf5f819c74ec40fbe0443bd301d9445f07092955
Gopinath V <57871800+GopinathV-1@users.noreply.github.com>
2021-06-11 20:41:55 +0530

    Update __init__.py

--- studentproj/__init__.py
+++ studentproj/__init__.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
